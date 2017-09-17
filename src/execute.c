89ab08bcfad653d9692a197373f4ddc1acd45b9c
ldercher <luke.dercher@gmail.com>
2017-09-17 13:54:53 -0500

    first commit

---
+++ src/execute.c
@@ -0,0 +1,339 @@
+/**
+ * @file execute.c
+ *
+ * @brief Implements interface functions between Quash and the environment and
+ * functions that interpret an execute commands.
+ *
+ * @note As you add things to this file you may want to change the method signature
+ */
+
+#include "execute.h"
+
+#include <stdio.h>
+
+#include "quash.h"
+#include "deque.h"
+
+struct{
+  
+
+}
+
+// Remove this and all expansion calls to it
+/**
+ * @brief Note calls to any function that requires implementation
+ */
+#define IMPLEMENT_ME()                                                  \
+  fprintf(stderr, "IMPLEMENT ME: %s(line %d): %s()\n", __FILE__, __LINE__, __FUNCTION__)
+
+/***************************************************************************
+ * Interface Functions
+ ***************************************************************************/
+
+// Return a string containing the current working directory.
+char* get_current_directory(bool* should_free) {
+  // Change this to true if necessary
+  *should_free = false;
+  char* cwd;
+  char cwd_buf[1024];
+  if((cwd = getcwd(cwd_buf, sizeof(cwd_buf))) == NULL)
+  {
+    perror("CWD error");
+  }
+  return cwd;
+}
+
+// Returns the value of an environment variable env_var
+const char* lookup_env(const char* env_var) {
+  char* env = getenv(env_var);
+  return env;
+}
+
+// Sets the environment variable env_var with the passed value val
+void write_env(const char* env_var, const char* val)
+{
+  setenv(env_var, val, 1);
+}
+
+// Check the status of background jobs
+void check_jobs_bg_status() {
+  // TODO: Check on the statuses of all processes belonging to all background
+  // jobs. This function should remove jobs from the jobs queue once all
+  // processes belonging to a job have completed.
+  //IMPLEMENT_ME();
+
+  //sizet_t numJobs;
+
+  //numbJobs = length_Jobs(&jobs);
+
+  // TODO: Once jobs are implemented, uncomment and fill the following line
+  // print_job_bg_complete(job_id, pid, cmd);
+}
+
+// Prints the job id number, the process id of the first process belonging to
+// the Job, and the command string associated with this job
+void print_job(int job_id, pid_t pid, const char* cmd) {
+  printf("[%d]\t%8d\t%s\n", job_id, pid, cmd);
+  fflush(stdout);
+}
+
+// Prints a start up message for background processes
+void print_job_bg_start(int job_id, pid_t pid, const char* cmd) {
+  printf("Background job started: ");
+  print_job(job_id, pid, cmd);
+}
+
+// Prints a completion message followed by the print job
+void print_job_bg_complete(int job_id, pid_t pid, const char* cmd) {
+  printf("Completed: \t");
+  print_job(job_id, pid, cmd);
+}
+
+/***************************************************************************
+ * Functions to process commands
+ ***************************************************************************/
+// Run a program reachable by the path environment variable, relative path, or
+// absolute path
+void run_generic(GenericCommand cmd) {
+  // Execute a program with a list of arguments. The `args` array is a NULL
+  // terminated (last string is always NULL) list of strings. The first element
+  // in the array is the executable
+  char* exec = cmd.args[0];
+  char** args = cmd.args;
+  execvp(exec, args);
+  //TODO##################################################################################
+  //perror("ERROR: Failed to execute program");
+}
+
+// Print strings
+void run_echo(EchoCommand cmd) {
+  // Print an array of strings. The args array is a NULL terminated (last
+  // string is always NULL) list of strings.
+  char** str = cmd.args;
+
+  for(int i=0; *(str+i) != NULL; i++)
+  {
+    printf("%s", *(str+i));
+  }
+  printf("\n");
+  // Flush the buffer before returning
+  fflush(stdout);
+}
+
+// Sets an environment variable
+void run_export(ExportCommand cmd) {
+  // Write an environment variable
+  const char* env_var = cmd.env_var;
+  const char* val = cmd.val;
+  write_env(env_var, val);
+}
+
+// Changes the current working directory
+void run_cd(CDCommand cmd) {
+  // Get the directory name
+  const char* dir = cmd.dir;
+
+  // Check if the directory is valid
+  if (dir == NULL) {
+    perror("ERROR: Failed to resolve path");
+    return;
+  }
+
+  if(chdir(dir) == -1)
+    printf("Path does not exist");
+  else
+  {
+    bool should_free = false;
+    write_env("PWD", get_current_directory(&should_free));
+  }
+
+}
+
+// Sends a signal to all processes contained in a job
+void run_kill(KillCommand cmd) {
+  int signal = cmd.sig;
+  int job_id = cmd.job;
+
+  if(kill(job_id, signal) == -1)
+    printf("unsuccessful Kill");
+}
+
+
+// Prints the current working directory to stdout
+void run_pwd() {
+
+  bool should_free = false;
+  printf("%s\n", get_current_directory(&should_free));
+
+  // Flush the buffer before returning
+  fflush(stdout);
+}
+
+// Prints all background jobs currently in the job list to stdout
+void run_jobs() {
+  // TODO: Print background jobs
+  IMPLEMENT_ME();
+
+  // Flush the buffer before returning
+  fflush(stdout);
+}
+
+/***************************************************************************
+ * Functions for command resolution and process setup
+ ***************************************************************************/
+
+/**
+ * @brief A dispatch function to resolve the correct @a Command variant
+ * function for child processes.
+ *
+ * This version of the function is tailored to commands that should be run in
+ * the child process of a fork.
+ *
+ * @param cmd The Command to try to run
+ *
+ * @sa Command
+ */
+void child_run_command(Command cmd) {
+  CommandType type = get_command_type(cmd);
+
+  switch (type) {
+  case GENERIC:
+    run_generic(cmd.generic);
+    break;
+
+  case ECHO:
+    run_echo(cmd.echo);
+    break;
+
+  case PWD:
+    run_pwd();
+    break;
+
+  case JOBS:
+    run_jobs();
+    break;
+
+  case EXPORT:
+  case CD:
+  case KILL:
+  case EXIT:
+  case EOC:
+    break;
+
+  default:
+    fprintf(stderr, "Unknown command type: %d\n", type);
+  }
+}
+
+/**
+ * @brief A dispatch function to resolve the correct @a Command variant
+ * function for the quash process.
+ *
+ * This version of the function is tailored to commands that should be run in
+ * the parent process (quash).
+ *
+ * @param cmd The Command to try to run
+ *
+ * @sa Command
+ */
+void parent_run_command(Command cmd) {
+  CommandType type = get_command_type(cmd);
+
+  switch (type) {
+  case EXPORT:
+    run_export(cmd.export);
+    break;
+
+  case CD:
+    run_cd(cmd.cd);
+    break;
+
+  case KILL:
+    run_kill(cmd.kill);
+    break;
+
+  case GENERIC:
+  case ECHO:
+  case PWD:
+  case JOBS:
+  case EXIT:
+  case EOC:
+    break;
+
+  default:
+    fprintf(stderr, "Unknown command type: %d\n", type);
+  }
+}
+
+/**
+ * @brief Creates one new process centered around the @a Command in the @a
+ * CommandHolder setting up redirects and pipes where needed
+ *
+ * @note Processes are not the same as jobs. A single job can have multiple
+ * processes running under it. This function creates a process that is part of a
+ * larger job.
+ *
+ * @note Not all commands should be run in the child process. A few need to
+ * change the quash process in some way
+ *
+ * @param holder The CommandHolder to try to run
+ *
+ * @sa Command CommandHolder
+ */
+void create_process(CommandHolder holder) {
+  // Read the flags field from the parser
+  bool p_in  = holder.flags & PIPE_IN;
+  bool p_out = holder.flags & PIPE_OUT;
+  bool r_in  = holder.flags & REDIRECT_IN;
+  bool r_out = holder.flags & REDIRECT_OUT;
+  bool r_app = holder.flags & REDIRECT_APPEND; // This can only be true if r_out
+                                               // is true
+
+  // TODO: Remove warning silencers
+  (void) p_in;  // Silence unused variable warning
+  (void) p_out; // Silence unused variable warning
+  (void) r_in;  // Silence unused variable warning
+  (void) r_out; // Silence unused variable warningo
+  (void) r_app; // Silence unused variable warning
+
+  // TODO: Setup pipes, redirects, and new process
+  IMPLEMENT_ME();
+
+  //parent_run_command(holder.cmd); // This should be done in the parent branch of
+                                  // a fork
+  //child_run_command(holder.cmd); // This should be done in the child branch of a fork
+}
+
+// Run a list of commands
+void run_script(CommandHolder* holders) {
+  if (holders == NULL)
+    return;
+
+  check_jobs_bg_status();
+
+  if (get_command_holder_type(holders[0]) == EXIT &&
+      get_command_holder_type(holders[1]) == EOC) {
+    end_main_loop();
+    return;
+  }
+
+  CommandType type;
+
+  // Run all commands in the `holder` array
+  for (int i = 0; (type = get_command_holder_type(holders[i])) != EOC; ++i)
+    create_process(holders[i]);
+
+  if (!(holders[0].flags & BACKGROUND)) {
+    // Not a background Job
+    // TODO: Wait for all processes under the job to complete
+    IMPLEMENT_ME();
+  }
+  else {
+    // A background job.
+    // TODO: Push the new job to the job queue
+    IMPLEMENT_ME();
+
+    // TODO: Once jobs are implemented, uncomment and fill the following line
+    // print_job_bg_start(job_id, pid, cmd);
+  }
+}
